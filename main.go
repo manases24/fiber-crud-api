42f3c66e33638afd643c5f5d7020c5801853eb9a
mnsh5 <hugo.orlando2050@gmail.com>
2024-03-25 19:52:26 -0300

    :rocket: add new updates

--- main.go
+++ main.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/mnsh5/fiber-crud-api/database"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	database.ConnectDB()
+
+	app := fiber.New()
+
+	app.Use(cors.New(cors.Config{
+		AllowOrigins: "http://localhost:5173",
+	}))
 }
